e9fc45f3c626e7a309f2d8cf770189d119b5d833
HxCodeWarrior <296946361@qq.com>
2024-12-17 10:28:48 +0800

    第二届“长城杯”WEB篇Hello_web的writeup

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/16/长城杯/","2024/12/16/hello-world/"];function toRandomPost(){
+var posts=["2024/12/16/第二届”长城杯“铁人三项(防护赛)介绍/","2024/12/16/hello-world/","2024/12/17/第二届长城杯WEB篇-hello_web/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
